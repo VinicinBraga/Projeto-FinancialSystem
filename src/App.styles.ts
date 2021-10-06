74e8b130ac94f1e9a1bcb3f65314221e8f6f25da
VinicinBraga <vinicius@bragamatta.com.br>
2021-10-06 16:32:09 -0300

    Iniciando projeto - Sistema Financeiro

---
+++ src/App.styles.ts
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const Container = styled.div``;
+export const Header = styled.div``;
+export const HeaderText = styled.h1``;
+export const Body = styled.div``;
