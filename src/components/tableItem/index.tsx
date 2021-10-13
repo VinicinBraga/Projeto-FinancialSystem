f1c6c273a72f7ce2acb43df7cc7037ea40bf5fc0
VinicinBraga <vinicius@bragamatta.com.br>
2021-10-13 14:45:39 -0300

    Finalizado parte de exibiçao dos itens na tabela.

--- src/components/tableItem/index.tsx
+++ src/components/tableItem/index.tsx
@@ -1,6 +1,7 @@
 import * as C from './styles';
 import { Item } from '../../types/Item'
 import { formatDate } from '../../helpers/dateFilter'
+import { categories} from '../../data/categories'
 type Props = {
   item: Item
 }
@@ -8,10 +9,21 @@ type Props = {
 export const TableItem = ({item}: Props) => {
   return (
     <C.TableLine>
-      <C.TableColum>{formatDate(item.date)}</C.TableColum>
-      <C.TableColum>{item.category}</C.TableColum>
-      <C.TableColum>{item.title}</C.TableColum>
-      <C.TableColum>R$ {item.value}</C.TableColum>
+      <C.TableColum>
+        {formatDate(item.date)}</C.TableColum>
+      <C.TableColum>
+        <C.Category color={categories[item.category].color}>
+          {categories[item.category].title}
+        </C.Category>
+      </C.TableColum>
+        <C.TableColum>
+          {item.title}
+        </C.TableColum>
+      <C.TableColum> 
+        <C.Value color={categories[item.category].expense ? 'red' : 'green'}>
+        R$ {item.value}
+        </C.Value>
+      </C.TableColum>
     </C.TableLine>
   );
 }
--- src/data/items.ts
+++ src/data/items.ts
@@ -3,5 +3,5 @@ export const items:Item[] = [
   { date: new Date(2021, 9, 6), category: 'salary', title: 'Salário', value: 4500 },
   { date: new Date(2021, 9, 15), category: 'food', title: 'McDonalds', value: 32.12 },
   { date: new Date(2021, 9, 25), category: 'food', title: 'Burger King', value: 29.80 },
-  { date: new Date(2021, 10, 4), category: 'rent', title: 'Aluguel do apartamento', value: 1800 },
+  { date: new Date(2021, 9, 4), category: 'rent', title: 'Aluguel do apartamento', value: 1800 },
 ];
